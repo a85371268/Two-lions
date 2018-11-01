7c8fa62c80cb361493b8446dc99224373e14bbc6
1902217131@qq.com <1902217131@qq.com>
2018-11-01 20:34:51 +0800

    feat:列表页懒加载

--- src/store/state/index.js
+++ src/store/state/index.js
@@ -1,8 +1,8 @@
 export default {
-  cart: window.localStorage.getItem('cart') ? JSON.parse(window.localStorage.getItem('cart')) : [],
+  cart: JSON.parse(window.localStorage.getItem('cart')) || [],
   currentId: 2,
   homeId: 1,
-  historys: window.localStorage.getItem('tl-history') ? window.localStorage.getItem('tl-history').split(',') : [],
-  loginName: window.localStorage.getItem('isLogin') ? window.localStorage.getItem('isLogin') : '',
+  historys: window.localStorage.getItem('tl-history').split(',') || [],
+  loginName: window.localStorage.getItem('isLogin') || '',
   backUrl: ''
 }
