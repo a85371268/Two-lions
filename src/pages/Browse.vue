c07eaca5b94e334b01062c71dbe7a5e0b5b8c9a5
a85371268 <963814565@qq.com>
2018-11-02 14:26:19 +0800

    fix:修复了浏览历史头没有固定的问题

--- src/pages/Browse.vue
+++ src/pages/Browse.vue
@@ -4,10 +4,12 @@
       <span><i class="icon iconfont icon-fanhui" @click="goBack"></i></span>
       <p class="title">浏览历史</p>
     </div>
-    <list-title v-if="browseId.length===0">还没有浏览记录(ง •̀_•́)ง</list-title>
-    <list-title v-else>心动不如行动(ง •̀_•́)ง</list-title>
-    <div class="tl-browse-list">
-      <list-item v-for="item in this.browseId" :key="item.id" :item="item"></list-item>
+    <div class="tl-browse-wrapper">
+      <list-title v-if="browseId.length===0">还没有浏览记录(ง •̀_•́)ง</list-title>
+      <list-title v-else>心动不如行动(ง •̀_•́)ง</list-title>
+      <div class="tl-browse-list">
+        <list-item v-for="item in this.browseId" :key="item.id" :item="item"></list-item>
+      </div>
     </div>
   </div>
 </template>
@@ -35,10 +37,15 @@ export default {
 
 <style lang="scss" scoped>
   $main-color: #f8e372;
+  .tl-browse-body{
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
   .tl-browse-header {
     height:40px;
+    width: 100%;
     background-color: $main-color;
-    position: relative;
     display: flex;
     >span{
       color:#666;
@@ -50,14 +57,18 @@ export default {
       flex: 1;
       line-height: 40px;
       color: #666;
+      font-size: 15px;
     }
   }
   .list-title{
     background: #f5f5f5;
   }
+  .tl-browse-wrapper{
+    flex: 1;
+    overflow-x: hidden;
+  }
   .tl-browse-list{
     background: #f5f5f5;
-    flex: 1;
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between
